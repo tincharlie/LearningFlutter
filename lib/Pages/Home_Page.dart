85581d81df345ac1a32a821b0ea36865c4dade29
user <vipulshukla999@gmail.com>
2022-11-13 17:25:36 +0530

    NewConmint

--- lib/Pages/Home_Page.dart
+++ lib/Pages/Home_Page.dart
@@ -10,6 +10,17 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("ML Class App"),
       ),
+      /* For App Bar Not using Here
+      AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "ML Class App",
+          style: TextStyle(color: Colors.black),
+        ),
+      )
+      */
       body: Center(
         child: Container(
           child: Text("Welcome to $days days of Machine Learning by $name"),
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -42,7 +42,7 @@ class MyDrawer extends StatelessWidget {
                 ),
               ),
             ),
-            
+
             ListTile(
               // onTap: (() => ,
               leading: Icon(
