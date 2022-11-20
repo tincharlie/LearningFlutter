4cbbf7b2ec49abc296ff27f7181d411a04c0352e
user <vipulshukla999@gmail.com>
2022-11-20 13:46:34 +0530

    day12Complete

--- lib/Pages/Home_Page.dart
+++ lib/Pages/Home_Page.dart
@@ -1,11 +1,16 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/mlapp.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Vipul";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("ML Class App"),
@@ -21,12 +26,26 @@ class HomePage extends StatelessWidget {
         ),
       )
       */
-      body: Center(
-        child: Container(
-          // child: Text(context.runtimeType.toString()),
-          child: Text("Welcome to $days days of Machine Learning by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
+
+      // Center(
+      //   // ignore: avoid_unnecessary_containers
+      //   child: Container(
+      //     // child: Text(context.runtimeType.toString()),
+      //     child: Text("Welcome to $days days of Machine Learning by $name"),
+      //   ),
+      // ),
+      // ignore: prefer_const_constructors
       drawer: MyDrawer(),
     );
   }
