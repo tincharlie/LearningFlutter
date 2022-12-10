dc0110e184e040fa74e249475d6cf82404d3ecfc
user <vipulshukla999@gmail.com>
2022-12-10 19:06:59 +0530

    Day23 Completed

---
+++ lib/models/cart.dart
@@ -0,0 +1,33 @@
+// ignore: unused_import
+// import 'package:flutter/material.dart';
+
+import 'package:flutter_application_1/widgets/home_widgets/catalog_list.dart';
+
+import 'mlapp.dart';
+
+class CartModel {
+  // ignore: unused_field
+  late CatalogModel _catalog;
+
+  final List<int> _itemIds = [];
+
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  Iterable get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
--- lib/models/mlapp.dart
+++ lib/models/mlapp.dart
@@ -4,10 +4,10 @@ class CatalogModel {
   static List<Item>? items;
 
   //Get Item by Id
-  static Item? getById(int id) =>
+  Item? getById(int id) =>
       items?.firstWhere((element) => element.id == id, orElse: null);
 
-  static Item getByPosition(int pos) =>items![pos];
+  Item getByPosition(int pos) =>items![pos];
 }
 
 class Item {
